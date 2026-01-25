ecfaef8cc47436f111e462402b6c4cab7a48a60e
Yerdos <qop6261@gmail.com>
2026-01-25 21:44:15 +0500

    feat: install shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
-import react from '@vitejs/plugin-react';
-import { URL, fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ['@babel/plugin-proposal-decorators', { legacy: true }],
-          ['@babel/plugin-transform-class-properties', { loose: true }],
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
