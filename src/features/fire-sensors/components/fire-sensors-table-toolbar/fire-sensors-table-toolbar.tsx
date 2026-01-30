16c3142978732d672a37196dbf24a0a52d47ed6b
Yerdos <qop6261@gmail.com>
2026-01-30 22:11:01 +0500

    feat: update table toolbar

--- src/features/fire-sensors/components/fire-sensors-table-toolbar/fire-sensors-table-toolbar.tsx
+++ src/features/fire-sensors/components/fire-sensors-table-toolbar/fire-sensors-table-toolbar.tsx
@@ -1,26 +1,36 @@
 import { Input } from "@/common/components/ui/input";
 import { FireSensorsTableToolbarProps } from "./fire-sensors-table-toolbar.types";
 import { Button } from "@/common/components/ui/button";
-import { PlusCircle } from "lucide-react";
+import { PlusCircle, Search } from "lucide-react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/common/components/ui/input-group";
 
 export function FireSensorsTableToolbar<TData>({
   table,
 }: FireSensorsTableToolbarProps<TData>) {
   return (
     <div className="flex gap-3 flex-col items-center mb-4">
-      <div className="w-full">
-        <Input
-          placeholder="Фильтр по серийному номеру"
-          value={
-            (table.getColumn("serialNumber")?.getFilterValue() as string) ?? ""
-          }
-          onChange={(event) =>
-            table.getColumn("serialNumber")?.setFilterValue(event.target.value)
-          }
-          className="w-full"
-        />
-      </div>
-      <div className="w-full flex justify-end">
+      <div className="w-full flex items-center gap-2">
+        <InputGroup>
+          <InputGroupInput
+            value={
+              (table.getColumn("serialNumber")?.getFilterValue() as string) ??
+              ""
+            }
+            onChange={(event) =>
+              table
+                .getColumn("serialNumber")
+                ?.setFilterValue(event.target.value)
+            }
+            placeholder="Фильтр по серийному номеру"
+          />
+          <InputGroupAddon>
+            <Search />
+          </InputGroupAddon>
+        </InputGroup>
         <Button>
           <PlusCircle />
           Добавить датчик
